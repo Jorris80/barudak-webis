b335937a6f97fd30e9fa8dfd9b908a01638859ed
Jorris80 <jorrisardhian98@gmail.com>
2026-07-10 18:47:34 +0700

    Update version to 'sawitgis-v2'
    Increment version number to trigger cache update.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *
  *  Naikkan VERSI setiap kali index.html diubah agar cache diperbarui.
  * ============================================================ */
-var VERSI = 'sawitgis-v1';
+var VERSI = 'sawitgis-v2';
 var INTI = [
   './',
   './index.html',
