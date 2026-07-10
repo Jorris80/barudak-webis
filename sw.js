0aa93e44a930ae6eb6ac8e374c31ed2bd65d7a74
Jorris80 <jorrisardhian98@gmail.com>
2026-07-10 19:23:10 +0700

    Increment version number to v4
    Updated the version number to v4 for cache refresh.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *
  *  Naikkan VERSI setiap kali index.html diubah agar cache diperbarui.
  * ============================================================ */
-var VERSI = 'sawitgis-v3';
+var VERSI = 'sawitgis-v4';
 var INTI = [
   './',
   './index.html',
