4ab41a71cbe7d67c343b3aecf131744802de20ee
Jorris80 <jorrisardhian98@gmail.com>
2026-07-12 04:04:36 +0700

    Update service worker version to v6

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *
  *  Naikkan VERSI setiap kali index.html diubah agar cache diperbarui.
  * ============================================================ */
-var VERSI = 'sawitgis-v5';
+var VERSI = 'sawitgis-v6';
 var INTI = [
   './',
   './index.html',
