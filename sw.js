c990dee75cef96907493af0ab36d902449f6a67b
Jorris80 <jorrisardhian98@gmail.com>
2026-07-12 14:30:53 +0700

    Bump version from v8 to v9
    Updated version number to trigger cache refresh.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *
  *  Naikkan VERSI setiap kali index.html diubah agar cache diperbarui.
  * ============================================================ */
-var VERSI = 'sawitgis-v8';
+var VERSI = 'sawitgis-v9';
 var INTI = [
   './',
   './index.html',
