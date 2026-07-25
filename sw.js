812a95b33f392dd5155600c8873ea3cd08e0f195
Jorris80 <jorrisardhian98@gmail.com>
2026-07-25 18:41:21 +0700

    Increment version number to v10
    Updated version number for cache management.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *
  *  Naikkan VERSI setiap kali index.html diubah agar cache diperbarui.
  * ============================================================ */
-var VERSI = 'sawitgis-v9';
+var VERSI = 'sawitgis-v10';
 var INTI = [
   './',
   './index.html',
