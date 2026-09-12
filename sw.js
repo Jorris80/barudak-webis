5b171a6c376787e4a2eca7ba7223cd33627732c3
Jorris80 <jorrisardhian98@gmail.com>
2026-09-12 23:18:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  *
  *  Naikkan VERSI setiap kali index.html diubah agar cache diperbarui.
  * ============================================================ */
-var VERSI = 'sawitgis-v3.7.2';
+var VERSI = 'sawitgis-v3.8.0';
 var INTI = [
   './',
   './index.html',
